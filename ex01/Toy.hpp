afeb9ae5da02e7fdba2033ceb49cb81d41df2777
Clément <clement2104.boillot@gmail.com>
2024-01-25 11:18:30 +0100

    Add task 1

---
+++ ex01/Toy.hpp
@@ -0,0 +1,41 @@
+/*
+** EPITECH PROJECT, 2024
+** ppool12
+** File description:
+** Toy
+*/
+
+#pragma once
+
+#include <string>
+
+#include "Picture.hpp"
+
+
+class Toy {
+   public:
+    using ToyType = enum {
+        BASIC_TOY,
+        ALIEN,
+    };
+
+    Toy();
+    Toy(Toy::ToyType type, std::string name, std::string filename);
+    Toy(const Toy& other) noexcept;
+    Toy(const Toy&& other) noexcept;
+    ~Toy();
+
+    Toy& operator=(const Toy& other);
+    Toy& operator=(Toy&& other) noexcept ;
+
+    Toy::ToyType getType() const;
+    std::string getName() const;
+    void setName(std::string name);
+    bool setAscii(std::string filename);
+    std::string getAscii() const;
+
+   private:
+    Toy::ToyType _type;
+    std::string _name;
+    Picture _picture;
+};
