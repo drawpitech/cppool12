fad7c44e23041ade7280681d0d4acdd39a1d9e73
Clément <clement2104.boillot@gmail.com>
2024-01-26 12:00:27 +0100

    Print the Bad Story BEFORE printing toys

--- ex06/ToyStory.cpp
+++ ex06/ToyStory.cpp
@@ -14,16 +14,15 @@ void ToyStory::tellMeAStory(
     std::string filename, Toy &toy1, bool (Toy::*func1)(std::string), Toy &toy2,
     bool (Toy::*func2)(std::string))
 {
-    std::cout << toy1.getAscii() << "\n" << toy2.getAscii() << "\n";
-
     std::ifstream file(filename);
     if (!file.is_open()) {
         std::cout << "Bad Story\n";
         return;
     }
 
-    std::string buf;
+    std::cout << toy1.getAscii() << "\n" << toy2.getAscii() << "\n";
 
+    std::string buf;
     for (std::size_t i = 0; getline(file, buf); i++) {
         auto toy = (i % 2 == 0) ? toy1 : toy2;
         auto method = (i % 2 == 0) ? func1 : func2;
